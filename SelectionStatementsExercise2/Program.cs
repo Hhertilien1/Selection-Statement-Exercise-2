b2af5360b53425f3d9e2fc29fd966a19335d2bea
Hhertilien1 <kinghertilien@gmail.com>
2023-12-13 15:49:09 -0500

    Completed part 2 of exercize

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I dislike math");
+                    break;
+                case "science":
+                    Console.WriteLine("science rules");
+                    break;
+                case "pe":
+                case "p.e.":
+                    Console.WriteLine("P.E. is awesome");
+                    break;
+                case "History":
+                    Console.WriteLine("History is my favorite too");
+                    break;
+                case "English":
+                    Console.WriteLine("English is important");
+                    break;
+                default:
+                    Console.WriteLine("I like that one too");
+                    break;
+            }
         }
     }
 }
